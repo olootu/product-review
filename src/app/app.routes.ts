6093783e037808677e1e86bc681545ca945df843
Ola Ogunyemi <ooguny04@student.bbk.ac.uk>
2025-04-02 21:32:16 +0100

    nav styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,12 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'beauty', component: BeautyComponent},
   {path: 'cars', component: CarsComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {
+    path: '**', redirectTo: 'home' ,pathMatch: 'full'
+  }
 ];
