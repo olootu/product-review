27f28b4b9fa3285d78e2c02b10237ee2cabef041
Ola Ogunyemi <ooguny04@student.bbk.ac.uk>
2025-04-03 16:04:21 +0100

    added more details to car page

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { SloganComponent } from '../slogan/slogan.component';
+import { CarComponent } from '../car/car.component';
 
 @Component({
   selector: 'app-cars',
   imports: [
     HeroComponent,
-    SloganComponent
+    SloganComponent,
+    CarComponent
   ],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
